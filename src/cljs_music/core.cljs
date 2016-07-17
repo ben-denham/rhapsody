0599333fa88fbb9cae19d9225316631c1dd11930
Benjamin Denham <ben.denham@gmail.com>
2016-07-17 20:51:13 +1200

    Initial commit.

---
+++ src/cljs_music/core.cljs
@@ -0,0 +1,124 @@
+(ns cljs-music.core
+  (:require
+        [cljs-bach.synthesis :as synthesis :refer
+         [audio-context run-with
+          destination current-time
+
+          connect-> add           ; Synth connectors
+
+          high-pass low-pass      ; Frequency filters
+
+          sawtooth sine square    ; Signal generators
+          triangle white-noise
+          constant
+
+          adsr gain percussive    ; Signal shapers
+          adshr envelope
+
+          stereo-panner reverb    ; Effects
+          delay-line enhance]]
+
+        [leipzig.scale :refer
+         [A B C D E F G           ; Keys
+          flat high low sharp     ; Key modifiers
+          major minor ionian      ; Scales
+          dorian phrygian lydian
+          mixolydian aeolian
+          locrian blues pentatonic
+          raise lower]]           ; Degree modifiers
+
+        [leipzig.chord :refer
+         [triad seventh ninth     ; Chords
+          inversion root augment]]; Chord modifiers
+
+        [leipzig.melody :refer
+         [phrase then times with  ; Melody makers
+          rhythm having
+          all bpm tempo where     ; Melody modifiers
+          after wherever duration]]
+
+        [leipzig.temperament :as temperament]))
+
+(defn play!
+  "Take a sequence of notes and play them in an audiocontext."
+  [audiocontext notes]
+  (doseq [{:keys [time duration instrument] :as note} notes]
+    (let [at (+ time (.-currentTime audiocontext))
+          synth-instance (-> note
+                             (update :pitch temperament/equal)
+                             (dissoc :time)
+                             instrument)
+          connected-instance (synthesis/connect synth-instance synthesis/destination)]
+      (connected-instance audiocontext at duration))))
+
+(defn ping [note]
+  (connect->
+   (square (:pitch note))
+   (adsr 0.01 0.1 1 1)
+   (low-pass (* 5 (:pitch note)))
+   (gain 0.1)))
+
+(defn bell [note]
+  (connect->
+   (sawtooth (:pitch note))
+   (adsr 0.01 0.1 1 0.5)
+   (low-pass (* 5 (:pitch note)))
+   (gain 0.3)))
+
+(defn bass-inst [note]
+  (connect->
+   (add (sine (:pitch note))
+        (sine (/ (:pitch note) 2)))
+   (adsr 0.5 0.5 0.5 16)
+   (gain 0.1)))
+
+(defn harmony [prev]
+  (->> (phrase (cycle [1])
+               (map #(-> triad (root %)) [0 4 5 3]))
+       (all :instrument ping)))
+
+(defn melody [prev]
+  (->> (phrase (cycle [0.5])
+               [0 2 4 0 5 7 3 5])
+       (all :instrument bell)))
+
+(defn bass [prev]
+  (->> (phrase [1]
+               [0])
+       (all :instrument bass-inst)))
+
+(defn live-fn [prev]
+  (->> (harmony prev)
+       (with (melody prev))
+       (with (bass prev))
+       (tempo (bpm 80))
+       (where :pitch (comp C major))))
+
+(defn loop!
+  ([context] (loop! context {}))
+  ([context prev]
+   (let [notes (live-fn prev)
+         delay (* 1000 (duration notes))]
+     (play! context notes)
+     (js/setTimeout #(loop! context notes) delay))))
+
+(defonce context (audio-context))
+(defonce main-loop
+  (loop! context))
+
+(defn midi-note-on [e]
+  (let [note (.-note e)
+        freq (temperament/equal (.-number note))]
+    (-> (ping {:pitch freq})
+        (connect-> destination)
+        (run-with context (current-time context) 1.0))))
+
+
+(.enable
+ js/WebMidi
+ (fn []
+   (let [inputs (.-inputs js/WebMidi)]
+     (doseq [input inputs]
+       (js/console.log input)
+       (.addListener input "noteon" "all"
+                     (fn [e] (midi-note-on e)))))))
