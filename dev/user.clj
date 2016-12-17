1404b17e064a4c654aeec3604c766cfea8b0a640
Benjamin Denham <ben.denham@gmail.com>
2016-12-17 20:32:38 +1300

    Add scripts to easily start figwheel server.

--- dev/user.clj
+++ dev/user.clj
@@ -19,24 +19,21 @@
 ;; and Component https://github.com/stuartsierra/component
 
 
-(defn fig-start
-  "This starts the figwheel server and watch based auto-compiler."
-  []
+(defn fig-start!
+  "This starts the figwheel server and repl with the watch based
+  auto-compiler."
+  [& {:keys [autobuild]
+      :or {autobuild true}}]
   ;; this call will only work are long as your :cljsbuild and
   ;; :figwheel configurations are at the top level of your project.clj
-  ;; and are not spread across different lein profiles
+  ;; and are not spread across different lein profilesotherwise you
+  ;; can pass a configuration into start-figwheel! manually
+  (f/start-figwheel!)
+  (when (not autobuild)
+    (f/stop-autobuild))
+  (f/cljs-repl))
 
-  ;; otherwise you can pass a configuration into start-figwheel! manually
-  (f/start-figwheel!))
-
-(defn fig-stop
-  "Stop the figwheel server and watch based auto-compiler."
-  []
-  (f/stop-figwheel!))
-
-;; if you are in an nREPL environment you will need to make sure you
-;; have setup piggieback for this to work
-(defn cljs-repl
-  "Launch a ClojureScript REPL that is connected to your build and host environment."
+(defn fig-repl
+  "This opens a new repl for a running figwheel server."
   []
   (f/cljs-repl))
